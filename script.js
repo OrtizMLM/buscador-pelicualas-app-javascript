5d04087477d8ab5ad6bdb98977720cf526c7e8fa
OrtizMLM <mauro.ortiz@alumnos.frm.utn.edu.ar>
2023-07-10 22:36:47 -0300

    Agregamos javascript para busqueda de peliculas y un loading

---
+++ script.js
@@ -0,0 +1,49 @@
+document.getElementById('searchButton').addEventListener('click', searchMovies)
+let claveApi = '0c9eccdc5702eefb1e8aa935de6c046e'
+let urlBase = 'https://api.themoviedb.org/3/search/movie'
+let urlImg = 'https://image.tmdb.org/t/p/w200'
+
+let resultContainer = document.getElementById('results')
+
+function searchMovies(){
+    resultContainer.innerHTML = 'Cargando...'
+    let searchInput = document.getElementById('searchInput').value
+    
+    fetch(`${urlBase}?query=${searchInput}&api_key=${claveApi}`)
+    .then(response => response.json())
+    .then(response => displayMovies(response.results))
+
+}
+
+function displayMovies(movies){
+    resultContainer.innerHTML = ''
+
+    if(movies.lenght === 0){
+        resultContainer.innerHTML = '<p> No se encontraron resultados para tu busqueda </p>'
+        return
+    }
+    movies.forEach(movie => {
+        let movieDiv = document.createElement('div')
+        movieDiv.classList.add('movie')
+
+        let title = document.createElement('h2')
+        title.textContent = movie.title
+
+        let releaseDate = document.createElement('p')
+        releaseDate.textContent = 'La fecha de lanzamiento fue: '+ movie.release_date
+
+        let overview = document.createElement('p')
+        overview.textContent = movie.overview  
+        let posterPath = urlImg + movie.poster_path
+        let poster = document.createElement('img')
+        poster.src = posterPath
+        
+
+        movieDiv.appendChild(poster)
+        movieDiv.appendChild(title)
+        movieDiv.appendChild(releaseDate)
+        movieDiv.appendChild(overview)
+
+        resultContainer.appendChild(movieDiv)
+    })
+}
